e7d981424aa0402953943b6c909b247d6d7ce68c
ShravanKonduru0 <shravankonduru2@gmail.com>
2022-11-03 16:34:26 -0500

    Update main.js

--- backend/frontend/static/main.js
+++ backend/frontend/static/main.js
@@ -92,18 +92,21 @@ $('#send_button').on('click', function (e) {
 
 	data = sendMessage();
 
-	fetch('http://127.0.0.1:5000/getMessage', {
+	async function getMessage() {
+		fetch('http://127.0.0.1:5000/getMessage', {
     	method: 'GET',
     	headers: {
         	'Accept': 'application/json',
     	},
-	})
-	.then(response => response.json().then(data => {
-		console.log(data);
-		botMessage = data['message'];
-	}))
-	
+		})
+		.then(response => response.json().then(data => {
+			console.log(data);
+			botMessage = data['message'];
+		}))
+	}
+
 	data = getMessage();
+	
 	$('#msg_input').val('');
 
 	// show bot message
