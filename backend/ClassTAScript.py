a8eecabb9e8cff1ff6b3fed1dd072774e5767a0e
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2022-11-03 16:23:16 -0500

    Trying out flask post and get

--- backend/ClassTAScript.py
+++ backend/ClassTAScript.py
@@ -2,26 +2,28 @@ import os, sys, json
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from flask_cors import CORS
 from flask import Flask, request, send_from_directory, jsonify
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-nlp = pipeline("question-answering", model = "..\ClassInfoTAPipeline")
+nlp = pipeline("question-answering", model = ".\ClassInfoTAPipeline")
 context_file = open('classInfo.txt','r')
-
-def AnswerQuestion(question):
-    QA_Input = {'question': question,
-            'context': context_file.read()}
-    
-    q = nlp(QA_Input)
-    if (q['score'] > 0.3): return 'Answer: ' + q['answer']
-    else: return 'Answer: I am not too confident on that question, please contact the TA for this class if you need more information'
+QA_Input = {'context': context_file.read()}
+q = nlp(QA_Input)
+message = ""
 
 @app.route('/postMessage', methods=['POST'])
 def postMessage():
     data = request.json
-#    message = AnswerQuestion(data['message'])
-    message = "test"
-    print(message)
-    return json.dumps({'message': message})
 
+    QA_Input['question'] = data['message']
+    q = nlp(QA_Input)
+    message = q['answer'] if (q['score'] > 0.3) else 'I am not too confident on that question, please contact the TA for this class if you need more information'
+
+    return jsonify({'message': message})
+
+@app.route('/getMessage', methods=['GET'])
+def getMessage():
+    print(message, file=sys.stdout)
+    return jsonify({'message': message})
