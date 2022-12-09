ebce1c1449936c19fbd5e3f3bde3a46f1cd59d00
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2022-12-09 13:23:50 -0600

    Delete Old Material TA
    We are using a new Material TA for our backend with rasa, so I delete the Python chatterbot code and added confidence to the JSON file we send back to the frontend.

--- backend/ClassTAScript.py
+++ backend/ClassTAScript.py
@@ -5,27 +5,14 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from flask_cors import CORS
 from flask import Flask, request, send_from_directory, jsonify
 
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-
 # Flask setup
 app = Flask(__name__)
 CORS(app)
 
 # CLASS-RELATED TA SETUP 
-nlp = pipeline("question-answering", model = ".\ClassInfoTAPipeline")
+nlp = pipeline("question-answering", model = "..\ClassInfoTAPipeline")
 context_file = open('classInfo.txt','r')
 QA_Input = {'context': context_file.read()}
-
-# MATERIAL-RELATED TA SETUP
-chatbot = ChatBot("VirtualTA")
-trainer = ListTrainer(chatbot)
-
-with open("CleanedWikis/chatbotdb.txt") as data:
-    for line in data:
-        if line == "\n": continue
-        qa = line.split("::")
-        trainer.train(qa)
 
 @app.route('/postClassMessage', methods=['POST'])
 def postClassMessage():
@@ -35,10 +22,7 @@ def postClassMessage():
     QA_Input['question'] = data['message']
     q = nlp(QA_Input)
     message = q['answer']
+    probability = q['score']
 
-    # Material message
-    if q['score'] < 0.3:
-        message = chatbot.get_response(data['message'])
-
-    return jsonify({'message': str(message)})
+    return jsonify({'message': str(message), 'probability': probability})
 
